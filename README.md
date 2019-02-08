df66dfff906744358af20d0400e2d8e706522d7a
LAPTOP-GE95V166\Janine <jliu99@seas.upenn.edu>
2019-02-07 21:23:24 -0500

    save progress

--- README.md
+++ README.md
@@ -5,8 +5,15 @@
 
 In addition to the class lectures and powerpoints, I consulted a few external resources for this project:
 - http://jamie-wong.com/2016/07/15/ray-marching-signed-distance-functions/#signed-distance-functions, for a summary of the ray-marching algorithm.
+- http://www.iquilezles.org/www/articles/distfunctions/distfunctions.htm, for a list of signed distance functions.
 
 # Live GitHub demo
 https://j9liu.github.io/hw2/
 
 # Features
+
+The background is a simple gradient between a pale yellow and green palette, linearly mixed with position of the fragments.
+
+![](background.png)
+
+The bowl was modeled using a capped cone SDF, where the depression in the bowl results from subtracting a smaller version of the volume.
--- src/shaders/flat-frag.glsl
+++ src/shaders/flat-frag.glsl
@@ -8,28 +8,39 @@ uniform float u_Time;
 in vec2 fs_Pos;
 out vec4 out_Col;
 
+const vec3 light_Vec = vec3(4.0, 6.0, 1.0);
+
 // Define color/shader IDs.
 #define BACKGROUND 0
+#define BOWL 1
+#define ICE_CREAM 2
+#define SPOON 3
 
-// Get the ray that goes through this fragment shader position
-vec3 rayCast() {
-  vec3 forward = normalize(u_Ref - u_Eye);
-  vec3 right = cross(forward, u_Up);
-  vec3 up = cross(right, forward);
-  float len = length(u_Ref - u_Eye);
-
-  vec3 v = up * len * tan(22.5f);
-  vec3 h = right * len * (u_Dimensions.x / u_Dimensions.y) * tan(22.5f);
-  vec3 p = u_Ref + fs_Pos.x * h + fs_Pos.y * v;
-  return normalize(p - u_Eye);
+bool floatEquality(float x, float y) {
+	return abs(x - y) < 0.0001;
 }
 
-// Define the SDFs of this scene
+/*
+ * GENERAL SDFs AND SDF OPERATIONS
+ */
+
 float sphereSDF(vec3 p, float r) {
 	return length(p) - r;
 }
 
-float bowlSDF(vec3 p, float h, float r1, float r2) {
+float boxSDF(vec3 p, vec3 b) {
+  vec3 d = abs(p) - b;
+  return length(max(d,0.0))
+         + min(max(d.x,max(d.y,d.z)),0.0); // remove this line for an only partially signed sdf 
+}
+
+float roundBoxSDF(vec3 p, vec3 b, float r) {
+  vec3 d = abs(p) - b;
+  return length(max(d,0.0)) - r
+         + min(max(d.x,max(d.y,d.z)),0.0); // remove this line for an only partially signed sdf
+}
+
+float cappedConeSDF(vec3 p, float h, float r1, float r2) {
 	vec2 q = vec2(length(p.xz), p.y);
     vec2 k1 = vec2(r2,h);
     vec2 k2 = vec2(r2 - r1, 2.0 * h);
@@ -43,6 +54,11 @@ float opUnion(float d1, float d2) {
 	return min(d1, d2);
 }
 
+float opSmoothUnion( float d1, float d2, float k ) {
+    float h = clamp( 0.5 + 0.5*(d2-d1)/k, 0.0, 1.0 );
+    return mix( d2, d1, h ) - k*h*(1.0-h); }
+
+
 float opSubtraction(float d1, float d2) {
 	return max(-d1, d2);
 }
@@ -51,8 +67,216 @@ float opIntersection(float d1, float d2) {
 	return max(d1, d2);
 }
 
+vec2 opRevolution(vec3 p, float w) {
+    return vec2( length(p.xz) - w, p.y );
+}
+
+/*
+ * MY SDFs
+ */
+
+float scoopSegment(vec3 p) {
+	mat4 transSphere = mat4(vec4(1.0, 0, 0, 0),
+					   		vec4(0, 1.0, 0, 0),
+					   		vec4(0, 0, 1.0, 0),
+					   		vec4(-2.8, -0.2, 0.0, 1.0));
+	return sphereSDF(vec3(transSphere * vec4(p, 1.0)), 1.2);
+}
+
+float scoopSDF(vec3 p) {
+	float d = scoopSegment(p);
+	for(float i = 30.0; i < 360.0; i += 30.0) {
+		mat4 rotSphere = mat4(vec4(cos(radians(i)), 0, sin(radians(i)), 0),
+						   	  vec4(0, 1.0, 0, 0),
+						   	  vec4(-sin(radians(i)), 0, cos(radians(i)), 0),
+						   	  vec4(0, 0, 0.0, 1.0));
+		d = opSmoothUnion(d, scoopSegment(vec3(rotSphere * vec4(p, 1.0))), .25);
+	}
+
+	mat4 transSphere = mat4(vec4(1.0, 0, 0, 0),
+					   		vec4(0, 1.0, 0, 0),
+					   		vec4(0, 0, 1.0, 0),
+					   		vec4(0, -.9, 0.0, 1.0));
+
+	d = opSmoothUnion(d, sphereSDF(vec3(transSphere * vec4(p, 1.0)), 2.8), .05);
+
+	mat4 transBox = mat4(vec4(1.0, 0, 0, 0),
+						 vec4(0, 1.0, 0, 0),
+						 vec4(0, 0, 1.0, 0),
+					 	 vec4(0.0, 4.33, 0, 1.0));
+	return opSubtraction(boxSDF(vec3(transBox * vec4(p, 1.0)), vec3(5.0, 5.0, 5.0)), d);
+}
+
+float bowlSDF(vec3 p) {
+	mat4 trans = mat4(vec4(1.0, 0, 0, 0),
+					  vec4(0, 1.0, 0, 0),
+					  vec4(0, 0, 1.0, 0),
+					  vec4(0, .1, 0, 1.0));
+	return opSubtraction(cappedConeSDF(p, 0.9, 2.1, 3.75),
+	cappedConeSDF(vec3(trans * vec4(p, 1.0)), 0.8, 2.2, 3.8));
+}
+
+float spoonHeadMoldSDF(vec3 p) {
+	mat4 trans = mat4(vec4(1.0, 0, 0, 0),
+					  vec4(0, 1.0, 0, 0),
+					  vec4(0, 0, 1.0, 0),
+					  vec4(0, 1.5, 0, 1.0));
+	return opIntersection(sphereSDF(p, 1.0), boxSDF(vec3(trans * vec4(p, 1.0)), vec3(0.75, 1.2, 1.4)));
+}
+
+float spoonHeadSDF(vec3 p) {
+	mat4 trans = mat4(vec4(1.0, 0, 0, 0),
+					  vec4(0, 1.0, 0, 0),
+					  vec4(0, 0, 1.0, 0),
+					  vec4(0, 0.2, 0, 1.0));
+	return opSubtraction(spoonHeadMoldSDF(p), spoonHeadMoldSDF(vec3(trans * vec4(p, 1.0))));	
+}
+
+float spoonHandleBendSDF(vec3 p) {
+    float c = cos(radians(8.0 * p.y));
+    float s = sin(radians(8.0 * p.y));
+    mat2  m = mat2(c, -s, s, c);
+    vec3  q = vec3(m * p.xy, p.z);
+    return boxSDF(q, vec3(0.06, 2.0, 0.3));
+}
+
+float spoonHandleSDF(vec3 p) {
+	mat4 translate = mat4(vec4(1.0, 0, 0, 0),
+						  vec4(0, 1.0, 0, 0),
+						  vec4(0, 0, 1.0, 0),
+						  vec4(0, -2.7, 0, 1.0));
+	mat4 rotY = mat4(vec4(cos(radians(90.0)), 0, sin(radians(90.0)), 0),
+					 vec4(0, 1.0, 0, 0),
+				     vec4(-sin(radians(90.0)), 0, cos(radians(90.0)), 0),
+				     vec4(0, 0, 0.0, 1.0));
+	mat4 rotZ = mat4(vec4(cos(radians(90.0)), -sin(radians(90.0)), 0, 0),
+				     vec4(sin(radians(90.0)), cos(radians(90.0)), 0, 0),
+				     vec4(0, 0, 1.0, 0),
+				     vec4(0, 0, 0.0, 1.0));
+	return spoonHandleBendSDF(vec3(translate * rotZ * rotY * vec4(p, 1.0)));
+}
+
+float spoonSDF(vec3 p) {
+	mat4 translate = mat4(vec4(1.0, 0, 0, 0),
+						  vec4(0, 1.0, 0, 0),
+						  vec4(0, 0, 1.0, 0),
+						  vec4(5.0, 0, 0, 1.0));
+	return opSmoothUnion(spoonHeadSDF(vec3(translate * vec4(p, 1.0))),
+						 spoonHandleSDF(vec3(translate * vec4(p, 1.0))), 0.224);
+}
+
 float sceneSDF(vec3 p) {
-    return bowlSDF(p, 1.0f, 1.0f, 5.0f);
+	return bowlSDF(p); //opUnion(spoonSDF(p), opUnion(bowlSDF(p), scoopSDF(p)));
+}
+
+int sceneColorID(vec3 p) {
+    float intersection = sceneSDF(p);
+    if(floatEquality(intersection, spoonSDF(p))) {
+    	return SPOON;
+    } else if(floatEquality(intersection, bowlSDF(p))) {
+    	return BOWL;
+    } else if(floatEquality(intersection, scoopSDF(p))) {
+    	return ICE_CREAM;
+    }
+
+    return BACKGROUND;
+} 
+
+/*
+ * NOISE / TOOLBOX FUNCTIONS
+ */
+
+float noise(float i) {
+	return fract(sin(vec2(203.311f * float(i), float(i) * sin(0.324f + 140.0f * float(i))))).x;
+}
+
+float random(vec2 p, vec2 seed) {
+  return fract(sin(dot(p + seed, vec2(127.1, 311.7))) * 43758.5453);
+}
+
+float interpNoise1D(float x) {
+	float intX = floor(x);	
+	float fractX = fract(x);
+
+	float v1 = noise(intX);
+	float v2 = noise(intX + 1.0f);
+	return mix(v1, v2, fractX);
+}
+
+float fbm(float x) {
+	float total = 0.0f;
+	float persistence = 0.5f;
+	int octaves = 8;
+	
+	for(int i = 0; i < octaves; i++) {
+		float freq = pow(2.0f, float(i));
+		float amp = pow(persistence, float(i));
+
+		total += interpNoise1D(x * freq) * amp;
+	}
+
+	return total;
+}
+
+float interpNoise2D(float x, float y) {
+	float intX = floor(x);
+	float fractX = fract(x);
+	float intY = floor(y);
+	float fractY = fract(y);
+
+	float v1 = random(vec2(intX, intY), vec2(0));
+	float v2 = random(vec2(intX + 1.0f, intY), vec2(0));
+	float v3 = random(vec2(intX, intY + 1.0f), vec2(0));
+	float v4 = random(vec2(intX + 1.0f, intY + 1.0f), vec2(0));
+
+	float i1 = mix(v1, v2, fractX);
+	float i2 = mix(v3, v4, fractX);
+	return mix(i1, i2, fractY);
+}
+
+float fbm2(vec2 p) {
+	float total = 0.0f;
+	float persistence = 0.5f;
+	int octaves = 8;
+
+	for(int i = 0; i < octaves; i++) {
+		float freq = pow(2.0f, float(i));
+		float amp = pow(persistence, float(i));
+
+		total += interpNoise2D(p.x * freq, p.y * freq) * amp;
+	}
+
+	return total;
+}
+
+float perturbedFbm(vec2 p) {
+      vec2 q = vec2(fbm2(p + vec2(0.0,0.0)),
+                    fbm2(p + vec2(5.2,1.3)));
+      vec2 r = vec2(fbm2(p + 4.0*q + vec2(9.7,9.2)),
+                    fbm2(p + 4.0*q + vec2(8.3,2.8)));
+      return fbm2( p + 4.0*r );
+}
+
+float gain(float x, float k) 
+{
+    float a = 0.5 * pow(2.0 * ((x < 0.5) ? x : 1.0-x), k);
+    return (x < 0.5) ? a : 1.0 - a;
+}
+
+/*
+ * RAY MARCHING
+ */
+
+vec3 rayCast() {
+  vec3 forward = normalize(u_Ref - u_Eye);
+  vec3 right = cross(forward, u_Up);
+  vec3 up = cross(right, forward);
+  float len = length(u_Ref - u_Eye);
+
+  vec3 v = up * len * tan(22.5f);
+  vec3 h = right * len * (u_Dimensions.x / u_Dimensions.y) * tan(22.5f);
+  vec3 p = u_Ref + fs_Pos.x * h + fs_Pos.y * v;
+  return normalize(p - u_Eye);
 }
 
 #define epsilon 0.0005f
@@ -65,18 +289,49 @@ vec3 getNormal(vec3 p) {
 					    - sceneSDF(vec3(p.x, p.y, p.z - epsilon))));
 }
 
-vec4 getColor(int id, vec3 position) {
+vec4 applyLambert(vec3 p, vec3 base) {
+	vec3 normal = getNormal(p);
+	float lambert = clamp(dot(normalize(normal), normalize(light_Vec)), 0.0, 1.0);
+	// Add ambient lighting
+	float ambientTerm = 0.2;
+	float lightIntensity = lambert + ambientTerm;
+	return vec4(clamp(base * lightIntensity, 0.0f, 1.0f), 1.0f);
+}
+
+vec4 applyBlinnPhong(vec3 p, vec3 base, float power) {
+	vec3 normal = getNormal(p);
+	vec4 lambert = applyLambert(p, base);
+	// Average the view / light vector
+    vec3 h_vector = (normalize(light_Vec) + normalize(u_Eye)) / 2.0f;
+	// Calculate specular intensity
+	float specularIntensity = max(pow(dot(h_vector, normal), power), 0.0);
+	return vec4(clamp(vec3(lambert.rgb + .2 * specularIntensity), 0.0f, 1.0f), 1.0f);
+}
+
+vec4 getColor(int id, vec3 p) {
 	switch(id) {
+		case BOWL:
+			vec3 blue = vec3(195.0, 222.0, 224.0) / 255.0;
+			return applyBlinnPhong(p, blue, 4.0f);
+		case ICE_CREAM:
+			vec3 gray = vec3(20.0, 20.0, 25.0) / 255.0;
+			vec3 marble = clamp(gray + vec3(pow(perturbedFbm(1.489 * sin(p.xz) + p.zy), 3.0f)), 0.0, 1.0);
+			marble = clamp((1.0 - marble) + vec3(86.0, 66.0, 50.0) / 255.0, 0.0, 1.0);
+			return applyLambert(p, marble);
+		case SPOON:
+			vec3 silver = vec3(101, 106, 107) / 255.0;
+			return applyBlinnPhong(p, silver, 6.0f);
 		case BACKGROUND:
-			return vec4(mix(vec3(fs_Pos.xy, fs_Pos.x) / 5.0f, vec3(25.0f, 17.0f, 51.0f) / 255.0, vec3(104.0f, 12.0f, 69.0f)/255.0f), 1.0f);
 		default:
-			return vec4(vec3(.4f), 1.0f);
+			return vec4(mix(vec3(255.0, 247.0, 226.0) / 255.0,
+							vec3(206.0, 226.0, 226.0) / 255.0,
+							vec3(fs_Pos.xy, fs_Pos.x * fs_Pos.y) / 2.0), 1.0);
 	}
 }
 
 // March along the ray
-#define max_steps 15
-#define cutoff 1000.0f
+#define max_steps 200	
+#define cutoff 1100.0f
 void march(vec3 direction) {
 	float t = 0.0f;
 	int temp = 1;
@@ -85,14 +340,14 @@ void march(vec3 direction) {
 		pos = u_Eye + t * direction;
 		float dist = sceneSDF(pos);
 		if(dist < 0.001) {
-			out_Col = getColor(1, pos);
+			out_Col = getColor(sceneColorID(pos), pos);
 			return;
 		}
 
 		t += dist;
 
 		if(t >= cutoff) {
-			out_Col = getColor(0, pos);
+			out_Col = getColor(BACKGROUND, pos);
 			return;
 		}
 	}
